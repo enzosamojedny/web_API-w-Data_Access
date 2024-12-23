77f248bce65a7f38cf8e35b02967187d272da6ae
enzo samojedny <enzo_8_8_4@hotmail.com>
2024-12-22 22:18:04 -0300

    feat: salt added to hashed password

--- BLL/Login.cs
+++ BLL/Login.cs
@@ -18,21 +18,39 @@ namespace BLL
             _configuration = configuration;
         }
 
+
+        //agregada salt: cabe destacar que todos los users tienen la misma salt, lo cual es un problema
+        //porque si usan la misma pass, tienen el mismo hash.
+        //en el futuro, deberia crear un hash unico por cada usuario creado, y que quede asociado a su cuenta
+        //eso podria hacerlo agregando un row en la tabla Users donde se cree un hash unico cuando se registre la cuenta
         public string EncryptSHA256(string text)
         {
+            string saltFromConfig = _configuration["Salt"];
+
+            byte[] salt;
+
+                salt = Convert.FromBase64String(saltFromConfig);
+
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            byte[] textWithSaltBytes = new byte[textBytes.Length + salt.Length];
+            Buffer.BlockCopy(textBytes, 0, textWithSaltBytes, 0, textBytes.Length);
+            Buffer.BlockCopy(salt, 0, textWithSaltBytes, textBytes.Length, salt.Length);
+
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+                byte[] hashBytes = sha256Hash.ComputeHash(textWithSaltBytes);
+
                 StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
+                foreach (byte b in hashBytes)
                 {
-                    builder.Append(bytes[i].ToString("x2"));
+                    builder.Append(b.ToString("x2"));
                 }
-                return builder.ToString();
+
+                return Convert.ToBase64String(salt) + ":" + builder.ToString();
             }
         }
 
-        public string GenerateJWT(User usuario)
+            public string GenerateJWT(User usuario)
         {
             var jwtKey = _configuration["JWT:Key"];
             var jwtIssuer = _configuration["JWT:Issuer"];
